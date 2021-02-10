9b22e77e415c5b34a6099d77cb8a2ad25b6b7496
Aaron Brezel <aaronbrezel@gmail.com>
2021-02-10 13:33:22 -0500

    adding email link

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -10,11 +10,11 @@ export default class Introduction extends React.Component {
         return (
             <div className="intro"> 
                 <p>My name is <span id="myName">Aaron Brezel</span>.</p>
-                <p>I am a Brookyln-based computational journalist and newsroom developer.</p>
+                <p>I am a Brooklyn-based computational journalist and newsroom developer.</p>
                 <p>I've worn lots of hats: investigative reporter, talk-radio host, data visualizationist and software engineer.</p>
                 <p>When out of the office, I like baseball, hiking, photography and video games.</p>
                 <p>I use this website to collect all my work links in one place.</p>
-                <p>Shoot me an email if you like to chat.</p>
+                <p>Shoot me <a  href="mailto:brezelaaron@gmail.com?subject=Hello from the website!" rel="noreferrer" target="_blank">an email</a> if you like to chat.</p>
                 <p>Happy browsing!</p>
             </div>
         );
